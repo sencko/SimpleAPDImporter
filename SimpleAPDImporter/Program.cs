43e9463da5707568103a9c55d3831d68efecb7b9
Aleksandar Aleksandrov <sencko@gmail.com>
2017-02-26 15:56:42 +0200

    minor changes

--- SimpleAPDImporter/Program.cs
+++ SimpleAPDImporter/Program.cs
@@ -23,25 +23,74 @@ namespace SimpleAPDImporter
             //^\s\d+(\.\w+)?\s{2,}((\S+\s)*)\s{2,}(((\-?\d*\.\d+)\s*)|(\*{2,}\s*))+$
             Regex parts = new Regex(@"^\s\d+(\.\w+)?\s{2,}((\S+\s)*)\s{2,}(((\-?\d*\.\d+)\s*)|(\*{2,}\s*))+");
             Regex splitter = new Regex(@"\s{2,}");
+            Regex newChapter = new Regex(@"^\s{2,}((?:\d+)(?:\.\w+)?)\s((?:\S+\s)*(?:\S)+)$");
 
             StreamReader reader = new FileInfo(filename).OpenText();
             string line;
-            var ExcelApp = new Excel.Application();
-            ExcelApp.
+            Excel.Application application = new Excel.Application();
+            application.Visible = true;
+            Excel.Workbook newWorkbook = application.Workbooks.Add();
+            Excel.Worksheet sheet = null;
+            int i = 0;
+            string chapterName = null;
+            string escapedChapterName = null;
             while ((line = reader.ReadLine()) != null)
             {
-                Match match = parts.Match(line);
-                if (match.Success)
+                if (newChapter.IsMatch(line))
                 {
-                  //  int number = int.Parse(match.Groups[1].Value);
-                   // string path = match.Groups[2].Value;
-                    Console.Out.WriteLine(line);
-                    string[] cells = splitter.Split(line);
-                    // At this point, `number` and `path` contain the values we want
-                    // for the current line. We can then store those values or print them,
-                    // or anything else we like.
+                    if ((chapterName == null) || !chapterName.Equals(line))
+                    {
+                        chapterName = line;
+                        sheet = newWorkbook.Worksheets.Add();
+                        escapedChapterName = escape(chapterName);
+                        sheet.Name = escapedChapterName;
+                        sheet.Activate();
+
+                    }
+                }
+                else
+                {
+                    if ((chapterName!=null) && (line.StartsWith(chapterName)))
+                    {
+                        // total check value
+
+                    }
+                    else
+                    {
+
+                        Match match = parts.Match(line);
+                        if (match.Success)
+                        {
+                            Console.Out.WriteLine(sheet.Name);
+                            string[] header = splitter.Split(line, 3);
+                            
+                            sheet.Cells[1][i + 1].Value = header[0].Trim();
+                            sheet.Cells[2][i + 1].Value = header[1].Trim();
+
+                            string[] values = header[2].Split(new char[]{ ' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+                            for (int j = 0; j < values.Length; j++)
+                            {
+                                sheet.Cells[j + 3][i + 1] = values[j].Trim();
+                            }
+                            i++;
+                        }
+                    }
                 }
             }
+        }
+
+        private string escape(string chapterName)
+        {
+            if (chapterName == null)
+            {
+                return "null";
+            }
+            string ret = chapterName.Replace(':', ' ').Replace('\\', ' ').Replace('/', ' ').Replace('?', ' ').Replace('*', ' ').Replace('[', ' ').Replace(']', ' ').Trim();
+            if (ret.Length > 30)
+            {
+                ret = ret.Substring(0, 30);
+            }
+            return ret;
         }
     }
 }
